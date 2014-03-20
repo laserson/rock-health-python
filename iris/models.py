684d0c158593779f7c352c147a996e7d4f3204de
Uri Laserson <laserson@cloudera.com>
2014-03-20 00:14:34 -0700

    error checking in iris

--- iris/models.py
+++ iris/models.py
@@ -1,4 +1,4 @@
-def predict_species(sepal_width, petal_length, petal_width):
+def predict_species_impala(sepal_width, petal_length, petal_width):
     """ Predictor for species from model/52952081035d07727e01d836
         Predictive model by BigML - Machine Learning Made Easy
     """
@@ -30,6 +30,43 @@ def predict_species(sepal_width, petal_length, petal_width):
                     return 1
     if (petal_width <= 0.8):
         return 2
+
+
+def predict_species_spark(sepal_width=None, petal_length=None, petal_width=None):
+    """ Predictor for species from model/52952081035d07727e01d836
+        Predictive model by BigML - Machine Learning Made Easy
+    """
+    # 0 == Iris-virginica
+    # 1 == Iris-versicolor
+    # 2 == Iris-setosa
+    try:
+        if (petal_width > 0.8):
+            if (petal_width <= 1.75):
+                if (petal_length > 4.95):
+                    if (petal_width <= 1.55):
+                        return 0
+                    if (petal_width > 1.55):
+                        if (petal_length > 5.45):
+                            return 0
+                        if (petal_length <= 5.45):
+                            return 1
+                if (petal_length <= 4.95):
+                    if (petal_width <= 1.65):
+                        return 1
+                    if (petal_width > 1.65):
+                        return 0
+            if (petal_width > 1.75):
+                if (petal_length > 4.85):
+                    return 0
+                if (petal_length <= 4.85):
+                    if (sepal_width <= 3.1):
+                        return 0
+                    if (sepal_width > 3.1):
+                        return 1
+        if (petal_width <= 0.8):
+            return 2
+    except Exception:
+        return -1
 
 
 
